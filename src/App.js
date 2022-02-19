f916d3809c81dd11868c6b692a5845bb472bc304
Johnny <52947117+Toastjpg@users.noreply.github.com>
2022-02-19 00:02:52 -0800

    initial css changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>sum.ai!</h1>
+      <h1>sum.ai</h1>
       <p>Quickly summarize your lectures into auto generated notes</p>
       <p>Upload File Below</p>
       <button>Upload</button>
