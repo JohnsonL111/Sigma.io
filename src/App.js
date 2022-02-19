374d8b9a07a8eeecf4bcedaa00e4632623c4a499
Johnny <52947117+Toastjpg@users.noreply.github.com>
2022-02-18 23:58:57 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import './App.css';
+
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>sum.ai!</h1>
+      <p>Quickly summarize your lectures into auto generated notes</p>
+      <p>Upload File Below</p>
+      <button>Upload</button>
     </div>
   );
 }
