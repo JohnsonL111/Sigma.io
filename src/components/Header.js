1a1bcb8a5bac5182dd84f0601972fed912ebdf78
Johnny <52947117+Toastjpg@users.noreply.github.com>
2022-02-19 17:44:57 -0800

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Header() {
   return (
     <div className="Header">
-      <h1 className='Header_Title'>sum.ai</h1>
+      <h1 className='Header_Title'>SigmaAI</h1>
     </div>
   )
 }
